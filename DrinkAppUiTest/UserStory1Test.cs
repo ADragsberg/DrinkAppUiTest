0b4e387dc1064af53ed6d98cf6fd978613baec06
Andreas Dragsberg <115148828+ADragsberg@users.noreply.github.com>
2023-11-28 09:46:27 +0100

    start

--- DrinkAppUiTest/UserStory1Test.cs
+++ DrinkAppUiTest/UserStory1Test.cs
@@ -3,6 +3,10 @@ namespace DrinkAppUiTest
     [TestClass]
     public class UserStory1Test
     {
+        [ClassInitialize]
+        // 
+        [ClassCleanup]
+        // 
         [TestMethod]
         public void TestMethod1()
         {
