51f9eb70c673fd3738c3679b83fb340bd965a8ef
TrisES <112662675+TrisES@users.noreply.github.com>
2023-11-28 09:52:39 +0100

    Nuget packages installed, and test boilerplate setup.

--- DrinkAppUiTest/UserStory2Test.cs
+++ DrinkAppUiTest/UserStory2Test.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Edge;
 
 namespace DrinkAppUiTest
 {
     [TestClass]
     public class UserStory2Test
     {
+        private static readonly string DriverDirectory = "C:\\webdrivers";
+        private static IWebDriver _driver;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context) // Nok vigtigt at den er static, har TestContext som parameter.
+        {
+            // _driver = new ChromeDriver(DriverDirectory);
+            //_driver = new FirefoxDriver(DriverDirectory);
+            _driver = new EdgeDriver(DriverDirectory);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
