7d99b96171104721f5dbbb2749c1c96e5fdc5eed
TrisES <112662675+TrisES@users.noreply.github.com>
2023-11-28 09:59:12 +0100

    UserStory boilerplate created.

---
+++ DrinkAppUiTest/UserStory3Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Edge;
+
+namespace DrinkAppUiTest
+{
+    [TestClass]
+    public class UserStory3Test
+    {
+        private static readonly string DriverDirectory = "C:\\webdrivers";
+        private static IWebDriver _driver;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context) // Nok vigtigt at den er static, har TestContext som parameter.
+        {
+            // _driver = new ChromeDriver(DriverDirectory);
+            //_driver = new FirefoxDriver(DriverDirectory);
+            _driver = new EdgeDriver(DriverDirectory);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
