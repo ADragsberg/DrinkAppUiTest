fd731a1a51b67a921d7929c7c2c7253c9c930183
daoudahmad <daoud@hotmail.dk>
2023-11-30 14:31:19 +0100

    Filter alkohol test

--- DrinkAppUiTest/UserStory4Test.cs
+++ DrinkAppUiTest/UserStory4Test.cs
@@ -59,6 +59,66 @@ namespace DrinkAppUiTest
             }
             Assert.AreEqual(25, listeKopi.Count);
         }
+        
+        [TestMethod]
+        public void TestAlkoholFilterJa()
+        {
+            string url = useLocal ? _localUrl : _onlineUrl;
+            
+            _driver.Navigate().GoToUrl(url);
+
+            WebDriverWait wdWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            
+            IWebElement ventPåListe = wdWait.Until(ventPåListe => ventPåListe.FindElement(By.TagName("li")));
+
+            ReadOnlyCollection<IWebElement> drinksListe = _driver.FindElements(By.ClassName("drinkNavne"));
+            Assert.AreEqual(25, drinksListe.Count);
+
+            List<string> listeKopi = new List<string>();
+            foreach (var drink in drinksListe)
+            {
+                listeKopi.Add(drink.Text);
+            }
+            for (int i = 0; i < listeKopi.Count; i++)
+            {
+                Assert.IsTrue(listeKopi[i].Length > 0);
+            }
+            Assert.AreEqual(25, listeKopi.Count);
+            
+            IWebElement filterMenu = _driver.FindElement(By.Id("filterMenu"));
+            filterMenu.Click();
+            
+            IWebElement filterAlcoholic = _driver.FindElement(By.Id("filterAlcoholic"));
+            filterAlcoholic.Click();
+            
+            ReadOnlyCollection<IWebElement> drinksListe2 = _driver.FindElements(By.ClassName("AlkoholJaNej"));
+            Assert.AreEqual(24, drinksListe2.Count);
+
+            List<string> listeKopi2 = new List<string>();
+            foreach (var drink in drinksListe2)
+            {
+                listeKopi2.Add(drink.Text);
+            }
+            for (int i = 0; i < listeKopi2.Count; i++)
+            {
+                Assert.IsTrue(listeKopi2[i].Length > 0);
+            }
+            Assert.AreEqual(24, listeKopi2.Count);
+            
+        }
+        
+        
+        
+        
+        
+        //Lad den være som den er til linje 83. du skal stadig finde alle elementer i listen nemlig 
+        //Linje 96 skal du ændre det til at finde Dropdown menuen. så den hedder filterMenu 
+        //også skal den klikke. 
+        //derefter skal du lave endnu en findelement med id filterAlcoholic
+        //og igen klikke på den.
+        //Derefter skriv noglelunde samme fra 100 til 113 
+        // alt dette tager udgangspunkt i Branch us4 og klasse us1test
+
 
     }
 }
